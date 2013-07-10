893141238bea055dfd15dbb1da058fe28f834ad4
War Master <warmaster@ubuntu.(none)>
2013-07-09 23:18:13 -0500

    Updated web.js with file read

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+var buf = fs.readFileSync('index.html', null, function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World!');
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.send(buf.toString('utf8'), 0, buf.length);
 });
 
 var port = process.env.PORT || 5000;
