966a4747964e26552a1a2ab82dbf037d9301b14c
War Master <warmaster@ubuntu.(none)>
2013-07-10 04:10:35 -0500

    Updated checks.json to include additional checks.  Updated grader.js to take either a file or a URL as an input on the command line.

--- grader.js
+++ grader.js
@@ -19,12 +19,18 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + restler
+   - https://github.com/danwrong/restler
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "http://war-zone-01.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,8 +42,34 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertURL = function(inuri) {
+    var apiurl = inuri.toString();
+    url2html(apiurl);
+    return true; // Needed to effectively ignore --file parameter
+};
+
+var url2html = function(inurl) {
+    rest.get(inurl).on('complete', function(result, response) {
+        if (result instanceof Error) {
+            console.log("%s generated an error. Exiting.\n%s", inurl, result.message);
+            process.exit(1);
+        } else if (response.statusCode == 200) {
+            program.url = result;
+            var checkJson = checkHtmlURL(program.url, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        } else {
+            console.log("%s resource could not be downloaded (%d). Exiting.\n%s", inurl, response.statusCode, result);
+        }
+    });
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioHtmlURL = function(htmlbuffer) {
+    return cheerio.load(htmlbuffer);
 };
 
 var loadChecks = function(checksfile) {
@@ -46,6 +78,17 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkHtmlURL = function(htmlbuffer, checksfile) {
+    $ = cheerioHtmlURL(htmlbuffer);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +108,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <uri_path>', 'Uniform Resource Locator [' + URL_DEFAULT + ']', clone(assertURL))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (!program.url) {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
